d6cce4b9b3544aaefb3bb0f04fbff40a099361af
Artem S <artemsimutin@gmail.com>
2022-11-15 18:34:59 +0200

    feat(styles): add custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
-import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
+import { trpc } from "../utils/trpc";
+import { Noto_Sans as NotoSans } from "@next/font/google";
+
+const notoSans = NotoSans({
+  weight: ["400", "500", "700"],
+  variable: "--font-noto_sans",
+  subsets: ['latin']
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +17,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${notoSans.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
