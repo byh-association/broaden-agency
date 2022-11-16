637df81a1723b3a6d3237d6705bfe485b305df76
Artem S <artemsimutin@gmail.com>
2022-11-16 19:05:59 +0200

    feat(landing): implement base hero section

---
+++ src/components/hero-section.tsx
@@ -0,0 +1,29 @@
+import type { FC } from "react";
+import React from "react";
+import HeroSectionIllustration from "@assets/hero-illustration.svg";
+
+const LandingHeroSection: FC = () => {
+  return (
+    <section className="my-24 flex w-full items-center justify-between space-y-24">
+      {/* Left text content */}
+      <div className="flex max-w-[560px] flex-col space-y-11">
+        <div className="flex flex-col">
+          <h1 className="mb-[-6px] text-8xl font-bold text-blue-500">Build</h1>
+          <h1 className="text-8xl font-bold text-slate-700">
+            your dream app with us
+          </h1>
+        </div>
+        <p className="text-2xl text-slate-700">
+          Our experience has helped our clients launch new companies in the
+          digital arena throughout the years.
+        </p>
+      </div>
+      {/* Hero section illustration */}
+      <div>
+        <HeroSectionIllustration />
+      </div>
+    </section>
+  );
+};
+
+export default LandingHeroSection;
