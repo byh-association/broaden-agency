57f3bc7c31a97d267520d437e977cf41b7b8ff25
Kristian Veter <rscrafter1234@icloud.com>
2022-11-17 13:09:46 +0200

    chore(pages): move from app dir to pages

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,5 +1,4 @@
 import type { FC, PropsWithChildren } from "react";
-import React from "react";
 
 const MainContainer: FC<PropsWithChildren> = ({ children }) => {
   return <div className="mx-auto w-full max-w-[1220px] px-5">{children}</div>;
