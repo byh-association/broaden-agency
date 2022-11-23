65e4e56a0766c4f4de9d455bc1f492c8172255cf
Artem Simutin <artemsimutin@gmail.com>
2022-11-23 18:46:16 +0200

    feat(advantages-section): implement advantages card and section

---
+++ src/component/advantages-section/advantage-card.type.ts
@@ -0,0 +1,11 @@
+export type AdvantageCardContent = {
+  readonly name: string;
+  readonly gradient: string;
+  readonly title: string;
+  readonly postTitle: string;
+  readonly description: string;
+  readonly image: {
+    url: string;
+    alt: string;
+  };
+};
