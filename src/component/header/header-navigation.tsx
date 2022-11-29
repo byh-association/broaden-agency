f31ad7ccc002c445d68c4514a28c40c2f8ee5ccd
Kristian Veter <rscrafter1234@icloud.com>
2022-11-29 11:46:21 +0200

    feat(components): implement header mobile menu

---
+++ src/component/header/header-navigation.tsx
@@ -0,0 +1,34 @@
+import type { FunctionalComponent } from "preact";
+
+import { getHeaderColorScheme } from "./helpers/get-header-color-scheme";
+
+interface Props {
+  isDark?: boolean;
+}
+
+export const HeaderNavigation: FunctionalComponent<Props> = ({ isDark }) => {
+  const { textColor, button } = getHeaderColorScheme(isDark);
+  return (
+    <>
+      <nav className="flex flex-col gap-6 sm:flex-row sm:gap-4">
+        <a href="#services" class={`font-medium ${textColor} scroll-smooth`}>
+          Services
+        </a>
+        <a class={`font-medium ${textColor}`}>Projects</a>
+        <a href="#contacts" class={`font-medium ${textColor} scroll-smooth`}>
+          Contacts
+        </a>
+      </nav>
+
+      <span className="hidden h-8 w-[1px] bg-slate-700 sm:block"></span>
+      <div className="flex space-x-2">
+        <a
+          className={`btn-small ${button.bgColor} ${button.textColor} rounded `}
+          href="/contact"
+        >
+          Estimate project
+        </a>
+      </div>
+    </>
+  );
+};
