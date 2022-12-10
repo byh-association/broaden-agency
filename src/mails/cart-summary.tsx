4c920675deff27bc2abe3ab1e9f25a6f7aaa6ec5
Artem Simutin <artemsimutin@gmail.com>
2022-12-10 15:46:16 +0200

    feat(mails): started client summary email

--- src/mails/cart-summary.tsx
+++ src/mails/cart-summary.tsx
@@ -1,25 +1,35 @@
-import React from "react";
-
 import type { Email } from "../utils/mailer";
 
 const CartSummaryEmail: Email = () => ({
   subject: "Thanks for your order",
   body: (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <p>Test email test</p>
-      <div
-        style={{
-          height: "100px",
-          width: "100px",
-          backgroundColor: "#ff0000",
-        }}
-      />
-    </div>
+    <html>
+      <head>
+        <style>
+          {`
+          * {
+              "box-sizing": border-box;
+              padding: 0;
+              margin: 0;
+              opacity: 0.5;
+              border: 1px solid red;
+          }
+          `}
+        </style>
+      </head>
+      <body>
+        <table
+          style={{
+            padding: "48px 24px",
+            backgroundColor: "#FAFAFA",
+            maxWidth: "600px",
+            width: "100%",
+          }}
+        >
+          <tr>some test text. enjoy)))</tr>
+        </table>
+      </body>
+    </html>
   ),
 });
 
